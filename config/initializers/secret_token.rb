5d69775d9dddaf0a7a43490c16ecc8451bf9d4eb
Franck Albinet <franckalbinet.gmail.com>
2013-09-06 11:15:20 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAdvizedOrg::Application.config.secret_token = '6bfac6fd0149337ecec89b131cef06292250e276a30c4df57e0e6683169ef66f91d983216da1f73a65421016167e5a89ec304ce8be59bc0e0f005c9267924398'
